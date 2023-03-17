c47214e23ee94f8d87f3eea798564b90866bae29
Kirstin <kpetersen@elon.edu>
2023-03-17 09:03:16 +0000

    responsvie navbar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Container } from "react-bootstrap";
 
 const Header = () => {
   const navigationLinks = [
@@ -6,22 +9,33 @@ const Header = () => {
     { title: "CountryPage", slug: "countries/:id" },
     { title: "AllCountries", slug: "/countries" },
     { title: "Sign Up", slug: "/signup" },
+    { title: "Login", slug: "/login" },
+    { title: "Sign Out", slug: "/sign-out" },
   ];
 
   return (
-    <div>
-      <header className="header">
-        <nav>
-          <ul>
-            {navigationLinks.map((link, idx) => (
-              <Link className="header_links" key={idx} to={link.slug}>
-                <li> {link.title}</li>
-              </Link>
-            ))}
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Desgin My Trip</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <ul>
+              {navigationLinks.map((link, idx) => (
+                <Nav.Link
+                  className="header_links"
+                  key={idx}
+                  as={Link}
+                  to={link.slug}
+                >
+                  <li>{link.title}</li>
+                </Nav.Link>
+              ))}
+            </ul>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
--- pages/AllCountries.jsx
+++ pages/AllCountries.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 const AllCountries = () => {
   return (
     <div>
-      <p>"You made it to the allCountries page."</p>
+      <p>You made it to the allCountries page.</p>
     </div>
   );
 };
