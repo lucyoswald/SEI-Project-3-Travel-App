46ed44c6a63ac9ce98a38fa5f9d33de9800f26a5
Kara Guarraci <karaguarraci@gmail.com>
2023-03-25 14:40:08 +0000

    activityButton

--- components/Activity.jsx
+++ components/Activity.jsx
@@ -6,6 +6,50 @@ import { Card, Button } from "react-bootstrap";
 
 const ActivityCard = ({ activity }) => {
   const [addedToItinerary, setAddedToItinerary] = useState(false);
+  const [showEditForm, setShowEditForm] = useState(false);
+
+  const initialFormData = {
+    category: activity.category,
+    name: activity.name,
+    location: activity.location,
+    description: activity.description,
+    price: activity.price,
+    imageUrl: activity.imageUrl,
+    linkToWebsite: activity.linkToWebsite,
+    activityId: activity._id,
+  };
+  const [formData, setFormData] = useState(initialFormData);
+  console.log(initialFormData);
+
+  const onClick = () => {
+    setFormData(initialFormData);
+    setShowEditForm(true);
+  };
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    console.log("update button clicked");
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem("token");
+      const updatedActivity = await axios.patch(
+        `${API_URL}/activities/${activity._id}`,
+        formData,
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+      console.log(updatedActivity);
+      setShowEditForm(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const addToItinerary = async (activityId) => {
     try {
@@ -46,15 +90,84 @@ const ActivityCard = ({ activity }) => {
           <span>{activity.description}</span> <br />
           <span> {activity.location}</span>
           <br /> <span>Cost: £{activity.price}</span>
+          <Button
+            onClick={() => addToItinerary(activity._id)}
+            variant="primary"
+            className="cardbutton"
+          >
+            {addedToItinerary ? "Added to Itinerary" : "Add to Itinerary"}
+          </Button>
+          {showEditForm ? (
+            <form className="activity-form" onSubmit={onSubmit}>
+              <select
+                name="category"
+                value={formData.category}
+                onChange={onChange}
+              >
+                <option value="">Select a category*</option>
+                <option value="Things to do">Things to do</option>
+                <option value="Things to see">Things to see</option>
+                <option value="Places to eat">Places to eat</option>
+              </select>
+              <input
+                placeholder="Activity Name*"
+                name="name"
+                value={formData.name}
+                onChange={onChange}
+              />{" "}
+              <input
+                placeholder="Location*"
+                name="location"
+                value={formData.location}
+                onChange={onChange}
+              />{" "}
+              <input
+                placeholder="Description"
+                name="description"
+                value={formData.description}
+                onChange={onChange}
+              />{" "}
+              <input
+                type="number"
+                placeholder="Price"
+                name="price"
+                value={formData.price}
+                onChange={onChange}
+              />{" "}
+              <input
+                placeholder="Image"
+                name="imageUrl"
+                value={formData.imageUrl}
+                onChange={onChange}
+              />{" "}
+              <input
+                placeholder="Link to site"
+                name="linkToWebsite"
+                value={formData.linkToWebsite}
+                onChange={onChange}
+              />
+              <Button
+                type="submit"
+                className="form-button"
+                // onClick={() => setShowForm(false)}
+              >
+                {" "}
+                Update
+              </Button>{" "}
+            </form>
+          ) : (
+            <Button className="cardbutton updateButton" onClick={onClick}>
+              Update
+            </Button>
+          )}
         </Card.Text>
-        <Button
+        {/* <Button
           onClick={() => addToItinerary(activity._id)}
           variant="primary"
           className="cardbutton"
-          // disabled={addedToItinerary}
         >
           {addedToItinerary ? "Added to Itinerary" : "Add to Itinerary"}
-        </Button>
+        </Button> */}
       </Card.Body>
     </Card>
   );
